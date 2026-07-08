69bc9366964ec1bf005319969ae6c56d9dd0ce10
Mtiga Digital online <omarymtiga07@gmail.com>
2026-07-09 00:07:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to Safari Stories!"
+    return render_template('index.html') # This loads your new HTML file
 
 if __name__ == '__main__':
     app.run(debug=True)
